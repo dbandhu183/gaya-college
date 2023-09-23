5c4a747a0a100a824ef0e13f8044e05a10d44e62
Deenbandhu kumar pandit <dbandhu183@gmail.com>
2023-09-23 22:43:04 +0530

    db

---
+++ cp1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=9;
+    if(a==9)
+    {
+        cout<<"nine"<<endl;
+    }
+    if(a>0)
+    {
+        cout<<"positive"<<endl;
+    }
+    else
+    {
+        cout<<"negative"<<endl;
+    }
+}
